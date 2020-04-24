01db23360913d9a85d4a8fddb261ef0d7ca1f8d6
Antoine Lebaud <freelance@antoine-lebaud.com>
2020-04-24 23:33:35 +0800

    add function get_matches_from_sift_descriptors to disparity file

--- stereo-vision/disparity.py
+++ stereo-vision/disparity.py
@@ -1,3 +1,22 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+def get_matches_from_sift_descriptors(img1, img2, flann_index_kdtree=0):
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+    index_params = dict(algorithm=flann_index_kdtree, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+    saved_matches = []
+    pts1 = []
+    pts2 = []
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.8 * n.distance:
+            saved_matches.append(m)
+            pts2.append(kp2[m.trainIdx].pt)
+            pts1.append(kp1[m.queryIdx].pt)
+    return saved_matches, pts1, pts2
