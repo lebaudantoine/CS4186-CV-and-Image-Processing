8059ae8f4f6487a7e83428d042a4afccffa770a4
Antoine Lebaud <freelance@antoine-lebaud.com>
2020-04-24 23:34:37 +0800

    add function draw_lines to disparity file

--- stereo-vision/disparity.py
+++ stereo-vision/disparity.py
@@ -20,3 +20,17 @@ def get_matches_from_sift_descriptors(img1, img2, flann_index_kdtree=0):
             pts2.append(kp2[m.trainIdx].pt)
             pts1.append(kp1[m.queryIdx].pt)
     return saved_matches, pts1, pts2
+
+
+def draw_lines(img1, img2, lines, pts1, pts2):
+    r, c = img1.shape
+    img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+        color = tuple(np.random.randint(0, 255, 3).tolist())
+        x0, y0 = map(int, [0, -r[2] / r[1]])
+        x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
+        img1 = cv2.line(img1, (x0, y0), (x1, y1), color, 8)
+        img1 = cv2.circle(img1, tuple(pt1), 12, color, -1)
+        img2 = cv2.circle(img2, tuple(pt2), 12, color, -1)
+    return img1, img2
